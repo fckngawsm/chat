8716b2765672ff6168ed46aeb7e5475b51a0775f
Kirill <marchenko@finleo.ru>
2024-05-26 02:25:42 +0300

    chore: update route

--- src/router/config.tsx
+++ src/router/config.tsx
@@ -1,4 +1,11 @@
 import { createBrowserRouter } from "react-router-dom";
+import { Chat } from "../components/Chat/Chat";
 import { authRoutes } from "./authRoutes";
 
-export const router = createBrowserRouter([...authRoutes]);
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Chat />,
+  },
+  ...authRoutes,
+]);
