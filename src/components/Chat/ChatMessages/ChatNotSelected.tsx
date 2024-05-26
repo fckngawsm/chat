94c7ed721aaba8f2c1ef315be9c1bf9f0e31b208
Kirill <marchenko@finleo.ru>
2024-05-27 00:08:46 +0300

    chore: replace to pages folder

--- src/components/Chat/ChatMessages/ChatNotSelected.tsx
+++
@@ -1,9 +0,0 @@
-import { ChatNotSelectedText } from "./styled";
-
-export const ChatNotSelected = () => {
-  return (
-    <ChatNotSelectedText>
-      Выберите чат чтобы отправить сообщение
-    </ChatNotSelectedText>
-  );
-};
