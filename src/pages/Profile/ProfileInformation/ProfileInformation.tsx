77f0f8b3c5d476fecc30c66d0f16be0b00636b1b
Kirill <marchenko@finleo.ru>
2024-05-28 23:23:20 +0300

    feat(ui): create ui part for upload page

--- src/pages/Profile/ProfileInformation/ProfileInformation.tsx
+++ src/pages/Profile/ProfileInformation/ProfileInformation.tsx
@@ -1,18 +1,27 @@
+import { useModal } from "../../../hooks/useModal";
+import { ModalAddAvatar } from "../../Chat/ChatMessages/Modal/ModalAddAvatar";
 import {
   StyledProfileInformation,
   StyledProfileInformationAvatar,
 } from "../styled";
-import { ProfileInformaitonStroke } from "./ProfileInformaitonStroke";
 import { ProfileInformationAction } from "./ProfileInformationAction";
+import { ProfileInformationStroke } from "./ProfileInformationStroke";
 import { StyledProfileInformationName } from "./styled";
 
 export const ProfileInformation = () => {
+  const { isOpen, modalRef, openModal } = useModal();
   return (
-    <StyledProfileInformation>
-      <StyledProfileInformationAvatar src="https://img.freepik.com/free-psd/3d-render-avatar-character_23-2150611728.jpg?size=338&ext=jpg&ga=GA1.1.2082370165.1716681600&semt=ais_user" />
-      <StyledProfileInformationName>Иван</StyledProfileInformationName>
-      <ProfileInformaitonStroke onlyRead />
-      <ProfileInformationAction />
-    </StyledProfileInformation>
+    <>
+      <StyledProfileInformation>
+        <StyledProfileInformationAvatar
+          onClick={openModal}
+          src="https://img.freepik.com/free-psd/3d-render-avatar-character_23-2150611728.jpg?size=338&ext=jpg&ga=GA1.1.2082370165.1716681600&semt=ais_user"
+        />
+        <StyledProfileInformationName>Иван</StyledProfileInformationName>
+        <ProfileInformationStroke onlyRead />
+        <ProfileInformationAction />
+      </StyledProfileInformation>
+      {isOpen && <ModalAddAvatar ref={modalRef} />}
+    </>
   );
 };
--- src/pages/Profile/ProfileSettings/ProfileSettings.tsx
+++ src/pages/Profile/ProfileSettings/ProfileSettings.tsx
@@ -1,4 +1,4 @@
-import { ProfileInformaitonStroke } from "../ProfileInformation/ProfileInformaitonStroke";
+import { ProfileInformationStroke } from "../ProfileInformation/ProfileInformationStroke";
 import {
   StyledProfileInformation,
   StyledProfileInformationAvatar,
@@ -7,10 +7,14 @@ import {
 
 export const ProfileSettings = () => {
   return (
-    <StyledProfileInformation>
-      <StyledProfileInformationAvatar src="https://img.freepik.com/free-psd/3d-render-avatar-character_23-2150611728.jpg?size=338&ext=jpg&ga=GA1.1.2082370165.1716681600&semt=ais_user" />
-      <ProfileInformaitonStroke />
-      <StyledProfileInformationButton>Сохранить</StyledProfileInformationButton>
-    </StyledProfileInformation>
+    <>
+      <StyledProfileInformation>
+        <StyledProfileInformationAvatar src="https://img.freepik.com/free-psd/3d-render-avatar-character_23-2150611728.jpg?size=338&ext=jpg&ga=GA1.1.2082370165.1716681600&semt=ais_user" />
+        <ProfileInformationStroke />
+        <StyledProfileInformationButton>
+          Сохранить
+        </StyledProfileInformationButton>
+      </StyledProfileInformation>
+    </>
   );
 };
